ed6b97e1d171ac3946c94bcb622133fe992a66b8
Kleber Pinel <kleberpinel@gmail.com>
2012-08-03 19:32:56 -0300

    assets

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -15,7 +15,9 @@ Ibnj::Application.configure do
   config.assets.compress = true
 
   # Don't fallback to assets pipeline if a precompiled asset is missed
-  config.assets.compile = false
+  config.assets.compile = true
+
+  config.assets.precompile += %w( *.css *.js )
 
   # Generate digests for assets URLs
   config.assets.digest = true
